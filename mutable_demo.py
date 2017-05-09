9b5b30437e12c18466f78733e62b5720f587a820
Dominique Toppani <dtoppani@twistbioscience.com>
2017-05-09 15:04:57 -0700

    Work in progress

--- mutable_demo.py
+++ mutable_demo.py
@@ -1,6 +1,12 @@
 from fields.mutables import MutableBase
 from fields.marshmallow_field import MarshmallowJSON
+from sqlalchemy import Column, String, create_engine
+from sqlalchemy.orm import Session, sessionmaker
 from marshmallow import Schema, fields, post_load
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.mutable import Mutable
+from sqlalchemy.dialects.postgresql import UUID
+from uuid import uuid4
 
 
 class MutableEngine(MutableBase):
@@ -40,5 +46,33 @@ Base = declarative_base()
 
 
 class Car(Base):
-    id = Column()
+    __tablename__ = 'car'
+    id = Column(UUID(as_uuid=True), primary_key=True, nullable=False)
+    name = Column(String)
     engine = Column(Mutable.as_mutable(MarshmallowJSON(EngineSchema)))
+
+
+def main():
+    engine = create_engine('postgresql://postgres:postgres@localhost:5432/pycon_marshmallow', echo=False)
+    session_maker = sessionmaker(bind=engine)
+    session: Session = session_maker()
+
+    car = Car('Ferrari')
+    car.id = uuid4()
+    car_id = car.id
+    car.engine = MutableEngine('ENG1234', 'V12')
+
+    session.add(car)
+    session.commit()
+    # expunge to prove query works
+    session.expunge_all()
+    car_copy = session.query(Car).get(car_id)
+    print(f'Car {car_copy.name} has a {car_copy.engine.configuration} with serial number {car_copy.engine.serial_number}')
+    car_copy.engine.configuration = 'V10'
+    session.commit()
+    car_copy2 = session.query(Car).get(car_id)
+    print(f'Car {car_copy.name} has a {car_copy.engine.configuration} with serial number {car_copy.engine.serial_number}')
+
+
+if __name__ == "__main__":
+    main()
